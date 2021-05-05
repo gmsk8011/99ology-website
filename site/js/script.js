3b866fa9dd1664f072812b5bbe4c1433d206cae7
99ology <99ology@gmail.com>
2021-05-06 01:21:14 +0530

    f

---
+++ site/js/script.js
@@ -0,0 +1,12 @@
+// var company = new  Object();
+// company.name = "99ology";
+// company.ceo = new Object();
+// company.ceo.firstname = "Prabal"
+// company.ceo.lastname = " Chowdhury"
+// console.log(company) 
+
+function Circle (radius){
+	console.log(this);
+}
+
+Circle();
